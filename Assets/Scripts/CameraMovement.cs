51bd291ff833da024fe9c5b373a9229038d5cee6
LauraWangQiu <yiwang03@ucm.es>
2024-11-26 14:30:11 +0100

    Cambio de escena aleatoria tras X segundos y más
    + Cambio de skybox según Día o Tarde
    + Crosshair
    + FadeIn-FadeOut visual entre días
    + Eventos con parámetros (luz de mesilla)
    + Eventos puntuales (teclado, clic)
    + Evento loopeado del reloj
    + Pantalla del PC con vídeo
    
    - Falta FadeIn-Out auditivo

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using FMODUnity;
 
 public class CameraMovement : MonoBehaviour
 {
     public float mouseSensitivity = 100.0f;
+    public float interactionDistance = 5f;
     private float xRotation = 0f;
     private float yRotation = 0f;
+
+    public EventReference lampEvent;
+    public EventReference keyboardEvent;
+    public EventReference clickEvent;
 
     void Start()
     {
@@ -13,6 +19,12 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        HandleMouseLook();
+        HandleInteraction();
+    }
+
+    void HandleMouseLook()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -20,7 +32,62 @@ public class CameraMovement : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         yRotation += mouseX;
-
         transform.localEulerAngles = new Vector3(xRotation, yRotation, 0f);
+    }
+
+    void HandleInteraction()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance))
+            {
+                if (hit.collider.CompareTag("Lamp"))
+                {
+                    Transform rootObject = hit.collider.transform.root;
+
+                    Transform spotLightTransform = rootObject.Find("Spot Light");
+                    if (spotLightTransform != null)
+                    {
+                        Light light = spotLightTransform.GetComponent<Light>();
+                        if (light != null)
+                        {
+                            light.enabled = !light.enabled;
+                            PlayLightSound(light.enabled);
+                        }
+                    }
+                } 
+                else if (hit.collider.CompareTag("Keyboard"))
+                {
+                    PlayKeyboardSound();
+                }
+                else if (hit.collider.CompareTag("Mouse"))
+                {
+                    PlayClickSound();
+                }
+            }
+        }
+    }
+
+    void PlayLightSound(bool isLightOn)
+    {
+        var instance = RuntimeManager.CreateInstance(lampEvent);
+        instance.setParameterByName("SwitchLight", isLightOn ? 1 : 0);
+        instance.start();
+        instance.release();
+    }
+
+    void PlayKeyboardSound()
+    {
+        var instance = RuntimeManager.CreateInstance(keyboardEvent);
+        instance.start();
+        instance.release();
+    }
+    void PlayClickSound()
+    {
+        var instance = RuntimeManager.CreateInstance(clickEvent);
+        instance.start();
+        instance.release();
     }
 }
---
+++ Assets/Scripts/RandomSceneLoader.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RandomSceneLoader : MonoBehaviour
+{
+    public float delayInSeconds = 10.0f;
+    public List<int> sceneIndexes;
+    private List<int> shuffledScenes;
+
+    public Canvas canvas;
+    public Image fadeImage;
+    public Image crosshair;
+    public float fadeDuration = 1.0f;
+
+    public GameObject myCamera;
+
+    void Awake()
+    {
+        myCamera.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+        ShuffleScenes();
+    }
+
+    public void StartLoadingScenes()
+    {
+        canvas.sortingOrder = 2;
+        myCamera.SetActive(true);
+        StartCoroutine(LoadScenesRandomly());
+    }
+
+    IEnumerator LoadScenesRandomly()
+    {
+        foreach (int sceneIndex in shuffledScenes)
+        {
+            yield return StartCoroutine(FadeIn());
+            SceneManager.LoadScene(sceneIndex);
+            yield return StartCoroutine(FadeOut());
+            yield return new WaitForSeconds(delayInSeconds);
+        }
+        // Anadir escena final
+    }
+
+    IEnumerator FadeIn()
+    {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < fadeDuration)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 1);
+        crosshair.color = new Color(crosshair.color.r, crosshair.color.g, crosshair.color.b, 0);
+    }
+
+    IEnumerator FadeOut()
+    {
+        crosshair.color = new Color(crosshair.color.r, crosshair.color.g, crosshair.color.b, 1);
+        float elapsedTime = fadeDuration;
+        while (elapsedTime > 0)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, elapsedTime / fadeDuration);
+            elapsedTime -= Time.deltaTime;
+            yield return null;
+        }
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 0);
+    }
+
+    private void ShuffleScenes()
+    {
+        shuffledScenes = new List<int>(sceneIndexes);
+        int n = shuffledScenes.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            int value = shuffledScenes[k];
+            shuffledScenes[k] = shuffledScenes[n];
+            shuffledScenes[n] = value;
+        }
+    }
+}
